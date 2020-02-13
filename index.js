ac38013f6978807747b2160f48a8a03a3e5ebfa6
XAVIRELAND <48772703+XAVIRELAND@users.noreply.github.com>
2020-02-13 22:16:07 +0000

    test

---
+++ index.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const Hapi = require('@hapi/hapi');
+
+const server = Hapi.server({
+    port: 3000,
+    host: 'localhost'
+});
+
+server.route(require('./routes'));
+
+async function init() {
+    await server.register(require('@hapi/inert'));
+    await server.start();
+    console.log(`Server running at: ${server.info.uri}`);
+}
+
+process.on('unhandledRejection', err => {
+    console.log(err);
+    process.exit(1);
+});
+
+init();
